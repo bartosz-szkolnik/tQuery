5b312876c1fc2d1878927bc069e99f930bf8e5f5
Bartosz Szkolnik <bartosz.szkolnik@protonmail.com>
2023-08-29 15:50:55 +0200

    Add the base class and some methods

---
+++ README.md
@@ -0,0 +1,16 @@
+# tQuery (an unnecessary copy of jQuery)
+
+Hi there! This is my attempt at this cool [article/tutorial](https://gomakethings.com/create-your-own-personal-jquery) which teaches you how [jQuery](https://api.jquery.com/) works under the hood by implementing it from scratch. In my attempt I also try to add TypeScript on top of it, because why not? 
+
+## Hot to run it
+
+- clone repository
+- Run `pnpm i`
+- Run `pnpm dev`
+- Enter `http://localhost:5173` in your browser
+
+
+Todo
+- [x] finish the article
+- [ ] add more methods from the library
+- [ ] have some fun with it
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,84 @@
-console.log('Hello mom!');
+type Selector = string;
+type Context = Element | Document;
+
+class $ {
+  private readonly elems: Element[];
+
+  constructor(selector: Selector, context: Context = document) {
+    this.elems = Array.from(context.querySelectorAll(selector));
+  }
+
+  find(selector: Selector) {
+    return new $(selector, this.elems[0]);
+  }
+
+  addClass(...classes: string[]) {
+    for (const item of this.elems) {
+      item.classList.add(...classes);
+    }
+
+    return this;
+  }
+
+  removeClass(...classes: string[]) {
+    for (const item of this.elems) {
+      item.classList.remove(...classes);
+    }
+
+    return this;
+  }
+
+  css(prop: Exclude<keyof CSSStyleDeclaration, 'parentRule' | 'length'>, val: string) {
+    for (const item of this.elems) {
+      const element = item as HTMLElement;
+      // @ts-expect-error
+      element.style[prop] = val;
+    }
+
+    return this;
+  }
+
+  attr(attr: string, val = '') {
+    for (const item of this.elems) {
+      item.setAttribute(attr, val);
+    }
+
+    return this;
+  }
+
+  hasAttr(attr: string) {
+    for (const item of this.elems) {
+      return item.hasAttribute(attr);
+    }
+  }
+
+  removeAttr(attr: string) {
+    for (const item of this.elems) {
+      item.removeAttribute(attr);
+    }
+  }
+
+  on(eventName: keyof HTMLElementEventMap, callback: (event: Event) => void) {
+    for (const item of this.elems) {
+      const element = item as HTMLElement;
+      element.addEventListener(eventName, callback);
+    }
+  }
+}
+
+const menu = new $('#menu');
+const elements = menu
+  .find('li.item')
+  .addClass('foo', 'bar')
+  .removeClass('bar')
+  .css('backgroundColor', 'rebeccapurple')
+  .css('color', '#fff');
+
+const hiddenParagraph = new $('#hidden');
+new $('#button').on('click', () => {
+  const isHidden = hiddenParagraph.hasAttr('hidden');
+  hiddenParagraph.attr(isHidden ? 'visible' : 'hidden');
+  hiddenParagraph.removeAttr(!isHidden ? 'visible' : 'hidden');
+});
+
+console.log(elements);
